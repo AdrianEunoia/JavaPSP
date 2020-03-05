3659fde60e992467bf9f9daa3c16af0d7e2b42c9
Adrián <45033738+AdrianEunoia@users.noreply.github.com>
2020-03-05 10:45:25 +0100

    Actu ex

--- src/Ejercicio2_Examen2/Controlador.java
+++ src/Ejercicio2_Examen2/Controlador.java
@@ -4,7 +4,7 @@ public class Controlador {
     public static void main(String[] args) {
         Parking parking = new Parking();
         for (int i = 1; i<16 ;i++){
-            Coche nuevoCoche = new Coche((int)Math.random()*10000000,i+"00 AAA",parking);
+            Coche nuevoCoche = new Coche((int)Math.random()*100000100,i+"00 AAA",parking);
             nuevoCoche.start();
         }
     }
